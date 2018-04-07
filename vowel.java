692048cd544a8b16ea952bb5db486ac2cab3e2cc
Jeevappk <32959841+Jeevappk@users.noreply.github.com>
2018-04-07 07:38:18 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,30 @@
+package github;
+import java.io.*;
+public class vowel {
+	public static void main(String[] args)throws Exception {
+		String option;
+		System.out.println("enter your choice:");
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		option=br.readLine();		
+		switch(option) {
+		case "a":
+			System.out.println("vowel");
+			break;
+		case "e":
+			System.out.println("vowel");
+			break;
+		case "i":
+			System.out.println("vowel");
+			break;
+		case "o":
+			System.out.println("vowel");
+			break;
+		case "u":
+			System.out.println("vowel");
+			break;
+		default:
+			System.out.println("not a vowel");
+			break;	
+		}
+	}
+}
